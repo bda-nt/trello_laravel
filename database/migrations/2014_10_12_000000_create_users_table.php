b883c647a9b2fd76a4f71feaf8fc082da57d0f5f
awsoremod <sotnikovilia@yandex.ru>
2022-10-29 18:38:56 +0500

    Изменил users миграцию, без создания новой миграци

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,9 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name', 32);
+            $table->string('surname', 64);
+            $table->string('login', 64)->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
