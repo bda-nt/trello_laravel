dc98df8c61353831f363edf03e51212b0c7dd9ed
bda-nt <74663073+bda-nt@users.noreply.github.com>
2022-11-05 12:34:38 +0500

    create empty crud: model, controllers, request

--- routes/backpack/custom.php
+++ routes/backpack/custom.php
@@ -16,4 +16,10 @@ Route::group([
     ),
     'namespace'  => 'App\Http\Controllers\Admin',
 ], function () { // custom admin routes
+    Route::crud('project', 'ProjectCrudController');
+    Route::crud('user2-project', 'User2ProjectCrudController');
+    Route::crud('task', 'TaskCrudController');
+    Route::crud('stage', 'StageCrudController');
+    Route::crud('priority', 'PriorityCrudController');
+    Route::crud('status', 'StatusCrudController');
 }); // this should be the absolute last line of this file
